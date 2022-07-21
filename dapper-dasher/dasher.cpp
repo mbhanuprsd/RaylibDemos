f5e560649b074705531320868db70c049fd0a319
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-07-21 14:54:33 +0530

    Dapper dasher - Lecture 50

--- dapper-dasher/dasher.cpp
+++ dapper-dasher/dasher.cpp
@@ -9,13 +9,35 @@ struct AnimData
     float runningTime;
 };
 
+bool isOnGround(AnimData data, int windowHeight)
+{
+    return data.pos.y >= windowHeight - data.rec.height;
+}
+
+AnimData updateAnimData(AnimData data, float deltaTime, int maxFrameCount)
+{
+    // update animation frame
+    data.runningTime += deltaTime;
+    if (data.runningTime >= data.updateTime)
+    {
+        data.runningTime = 0;
+        data.rec.x = data.frame * data.rec.width;
+        data.frame++;
+        if (data.frame > maxFrameCount - 1)
+        {
+            data.frame = 0;
+        }
+    }
+    return data;
+}
+
 int main()
 {
     // Window dimensions
-    const int windowWidth{640}, windowHeight{480};
+    const int windowDimensions[2] = {640, 480};
 
     // Initialize th windows
-    InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
+    InitWindow(windowDimensions[0], windowDimensions[1], "Dapper Dasher!");
 
     const int gravity{1000}; // Gravity - (pixels/s)/s
 
@@ -23,7 +45,7 @@ int main()
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     AnimData scarfyData{
         {0, 0, scarfy.width / 6, scarfy.height},
-        {windowWidth / 2 - scarfy.width / 12, windowHeight - scarfy.height},
+        {windowDimensions[0] / 2 - scarfy.width / 12, windowDimensions[1] - scarfy.height},
         0,
         1.0 / 12.0,
         0.0};
@@ -34,20 +56,20 @@ int main()
 
     // Nebula's parameters
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-    AnimData neb1Data{
-        {0.0, 0.0, nebula.width / 8, nebula.height / 8},
-        {windowWidth, windowHeight - nebula.height / 8},
-        0,
-        1.0 / 12.0,
-        0.0};
-
-    AnimData neb2Data{
-        {0.0, 0.0, nebula.width / 8, nebula.height / 8},
-        {windowWidth * 1.7, windowHeight - nebula.height / 8},
-        0,
-        1.0 / 16 / 0,
-        0.0};
-
+    const int nebulaeSize{6};
+    AnimData nebulae[nebulaeSize];
+    for (int i = 0; i < nebulaeSize; i++)
+    {
+        nebulae[i].rec.x = 0;
+        nebulae[i].rec.y = 0;
+        nebulae[i].rec.width = nebula.width / 8;
+        nebulae[i].rec.height = nebula.height / 8;
+        nebulae[i].pos.x = windowDimensions[0] + i * 400;
+        nebulae[i].pos.y = windowDimensions[1] - nebula.height / 8;
+        nebulae[i].frame = 0;
+        nebulae[i].runningTime = 0;
+        nebulae[i].updateTime = 1.0 / 12.0;
+    }
     const int nebVel{-300}; // Velocity - pixel/s
 
     SetTargetFPS(60);
@@ -61,11 +83,11 @@ int main()
         ClearBackground(WHITE);
 
         // Perform ground check for scarfy
-        if (scarfyData.pos.y >= windowHeight - scarfyData.rec.height)
+        if (isOnGround(scarfyData, windowDimensions[1]))
         {
             // Rectangle is on ground
             velocity = 0;
-            scarfyData.pos.y = windowHeight - scarfyData.rec.height;
+            scarfyData.pos.y = windowDimensions[1] - scarfyData.rec.height;
             isInAir = false;
         }
         else
@@ -76,18 +98,6 @@ int main()
             isInAir = true;
         }
 
-        // Perform out of screen for nebula
-        if (neb1Data.pos.x < -neb1Data.rec.width)
-        {
-            neb1Data.pos.x = windowWidth;
-        }
-
-        // Perform out of screen for second nebula
-        if (neb2Data.pos.x < -neb2Data.rec.width)
-        {
-            neb2Data.pos.x = windowWidth;
-        }
-
         // Jump on key press and when on ground
         if (IsKeyPressed(KEY_SPACE) && !isInAir)
         {
@@ -96,62 +106,26 @@ int main()
 
         // update scarfy position
         scarfyData.pos.y += velocity * dT;
-        // update nebula position
-        neb1Data.pos.x += nebVel * dT;
-        // update second nebula position
-        neb2Data.pos.x += nebVel * dT;
 
-        // update scarfy animation frame
         if (!isInAir)
         {
-            scarfyData.runningTime += dT;
-            if (scarfyData.runningTime >= scarfyData.updateTime)
-            {
-                scarfyData.runningTime = 0;
-                // Update scarfy animation frame
-                scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;
-                scarfyData.frame++;
-                if (scarfyData.frame > 5)
-                {
-                    scarfyData.frame = 0;
-                }
-            }
+            scarfyData = updateAnimData(scarfyData, dT, 6);
         }
 
-        // update nebula animation frame
-        neb1Data.runningTime += dT;
-        if (neb1Data.runningTime>= neb1Data.updateTime)
-        {
-            neb1Data.runningTime = 0;
-            // Update nebula animation frame
-            neb1Data.rec.x = neb1Data.frame * neb1Data.rec.width;
-            neb1Data.frame++;
-            if (neb1Data.frame > 7)
-            {
-                neb1Data.frame = 0;
-            }
-        }
-
-        // update second nebula animation frame
-        neb2Data.runningTime += dT;
-        if (neb2Data.runningTime >= neb2Data.updateTime)
-        {
-            neb2Data.runningTime = 0;
-            // Update nebula animation frame
-            neb2Data.rec.x = neb2Data.frame * neb2Data.rec.width;
-            neb2Data.frame++;
-            if (neb2Data.frame > 7)
-            {
-                neb2Data.frame = 0;
-            }
-        }
-
-        // draw nebula
-        DrawTextureRec(nebula, neb1Data.rec, neb1Data.pos, WHITE);
-        // draw second nebula
-        DrawTextureRec(nebula, neb2Data.rec, neb2Data.pos, BLUE);
         // draw scarfy
         DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+
+        // Loop for nebulae
+        for (int i = 0; i < nebulaeSize; i++)
+        {
+            // update nebula position
+            nebulae[i].pos.x += nebVel * dT;
+
+            nebulae[i] = updateAnimData(nebulae[i], dT, 8);
+
+            // draw nebula
+            DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, WHITE);
+        }
 
         EndDrawing();
     }
